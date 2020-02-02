2a306f79b99eed72216bb5c28ce5ab9b7998a5ee
Genevieve <genevievepc47@gmail.com>
2020-02-02 00:12:13 -0500

    Finished!

--- app/src/main/java/edu/temple/knowyourrentalrights/MainActivity.java
+++ app/src/main/java/edu/temple/knowyourrentalrights/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private Button button2;
     private Button emergencyButton;
+    private Button domesticButton;
+    private Button rentButton;
+    private Button helpButton;
+    private Button disableButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +45,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button = (Button) findViewById(R.id.button10);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openDomestic();
+            }
+        });
+
+        button = (Button) findViewById(R.id.button16);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openRent();
+            }
+        });
+
+        button = (Button) findViewById(R.id.button13);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openHelp();
+            }
+        });
+
+        button = (Button) findViewById(R.id.button14);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openDisable();
+            }
+        });
+
+
+
     }//end on create
+
+    public void openDisable()
+    {
+        Intent intent = new Intent(this, helpActivity.class);
+        startActivity(intent);
+    }
+
+    public void openHelp()
+    {
+        Intent intent = new Intent(this, helpActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRent()
+    {
+        Intent intent = new Intent(this, rentActivity.class);
+        startActivity(intent);
+    }
+
+    public void openDomestic()
+    {
+        Intent intent = new Intent(this, domesticActivity.class);
+        startActivity(intent);
+    }
 
     public void openmakeComplaint()
     {
